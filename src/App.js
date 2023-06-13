18db63830b7d45792de68a3d625a5b57ca2db4a6
ditobprasetio <ditoprasetio28@gmail.com>
2023-06-13 14:49:24 +0700

    final chat app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import FormSection from "./components/Form";
 import AnswerSection from "./components/Answer";
-// import { Text } from "@chakra-ui/react";
 
 function App() {
     const [answers, setAnswers] = useState("");
